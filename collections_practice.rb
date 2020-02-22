8d4bfb89adccd29c07321e8470245fbf4e91d27c
Simo Khouili <m.simokhouili@gmail.com>
2020-02-22 19:04:16 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,24 @@
+def sort_array_asc (array)
+  array.sort
+end
 
+def sort_array_desc(array)
+array.sort {|first_num, second_num| second_num <=> first_num}
+end
+
+def sort_array_char_count(array)
+array.sort_by {|string| string.length}
+end
+
+def swap_elements(array)
+array.sort_by {|idx| idx[2] <=> idx[0]}
+end
+
+
+def reverse_array(array)
+array.reverse
+end
+
+def kesha_maker(array)
+array
+end 
